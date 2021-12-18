00d41e36d27e2d58addebb2abcdd3e1e1afe9910
t-jian <824310991@qq.com>
2021-12-18 19:51:54 +0800

    Add files via upload

--- theme/ueditor-1.4.3/ueditor.config.js
+++ theme/ueditor-1.4.3/ueditor.config.js
@@ -1,35 +1,13 @@
-/**
- * ueditor完整配置项
- * 可以在这里配置整个编辑器的特性
- */
-/**************************提示********************************
- * 所有被注释的配置项均为UEditor默认值。
- * 修改默认配置请首先确保已经完全明确该参数的真实用途。
- * 主要有两种修改方案，一种是取消此处注释，然后修改成对应参数；另一种是在实例化编辑器时传入对应参数。
- * 当升级编辑器时，可直接使用旧版配置文件替换新版配置文件,不用担心旧版配置文件中因缺少新功能所需的参数而导致脚本报错。
- **************************提示********************************/
-
 (function() {
 
-    /**
-     * 编辑器资源文件根路径。它所表示的含义是：以编辑器实例化页面为当前路径，指向编辑器资源文件（即dialog等文件夹）的路径。
-     * 鉴于很多同学在使用编辑器的时候出现的种种路径问题，此处强烈建议大家使用"相对于网站根目录的相对路径"进行配置。
-     * "相对于网站根目录的相对路径"也就是以斜杠开头的形如"/myProject/ueditor/"这样的路径。
-     * 如果站点中有多个不在同一层级的页面需要实例化编辑器，且引用了同一UEditor的时候，此处的URL可能不适用于每个页面的编辑器。
-     * 因此，UEditor提供了针对不同页面的编辑器可单独配置的根路径，具体来说，在需要实例化编辑器的页面最顶部写上如下代码即可。当然，需要令此处的URL等于对应的配置。
-     * window.UEDITOR_HOME_URL = "/xxxx/xxxx/";
-     */
+
     window.UEDITOR_HOME_URL = "/";
     var URL = window.UEDITOR_HOME_URL || getUEBasePath();
-    /**
-     * 配置项主体。注意，此处所有涉及到路径的配置别遗漏URL变量。
-     */
+
     window.UEDITOR_CONFIG = {
 
-        //为编辑器实例添加一个路径，这个不能被注释
         UEDITOR_HOME_URL: URL
 
-        // 服务器统一请求接口路径
         ,
         serverUrl: URL + "net/controller.ashx",
         toolbars: [
@@ -171,8 +149,9 @@
         //    'square' : ''   //'■ 小方块'
         //}
         //,listDefaultPaddingLeft : '30'//默认的左边缩进的基数倍
-        //,listiconpath : 'http://bs.baidu.com/listicon/'//自定义标号的路径
-        //,maxListLevel : 3 //限制可以tab的级数, 设置-1为不限制
+        ,
+        listiconpath: 'https://cdn.jsdelivr.net/gh/t-jian/static/theme/ueditor-1.4.3/listicon/' //自定义标号的路径
+            //,maxListLevel : 3 //限制可以tab的级数, 设置-1为不限制
 
         //,autoTransWordToList:false  //禁止word中粘贴进来的列表自动变成列表标签
 
@@ -296,7 +275,8 @@
         //,toolbarTopOffset:400
 
         //设置远程图片是否抓取到本地保存
-        //,catchRemoteImageEnable: true //设置是否抓取远程图片
+        ,
+        catchRemoteImageEnable: true //设置是否抓取远程图片
 
         //pageBreakTag
         //分页标识符,默认是_ueditor_page_break_tag_
@@ -342,9 +322,9 @@
 
         //iframeUrlMap
         //dialog内容的路径 ～会被替换成URL,垓属性一旦打开，将覆盖所有的dialog的默认路径
-        //,iframeUrlMap:{
-        //    'anchor':'~/dialogs/anchor/anchor.html',
-        //}
+        // ,iframeUrlMap: {
+        //      'anchor': '~/dialogs/anchor/anchor.html',
+        // }
 
         //allowLinkProtocol 允许的链接地址，有这些前缀的链接地址不会自动添加http
         //, allowLinkProtocols: ['http:', 'https:', '#', '/', 'ftp:', 'mailto:', 'tel:', 'git:', 'svn:']
